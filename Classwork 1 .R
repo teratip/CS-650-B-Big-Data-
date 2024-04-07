0d7d60ea52a0ed16b61bc419f6fcc61b70123c0d
teratip <teratipallavi123@gmail.com>
2024-04-06 23:40:54 -0400

    first commit

---
+++ Classwork 1 .R
@@ -0,0 +1,79 @@
+# printed Hello World
+"Hello, World"
+
+# Trying to make and assign variables
+A <-5
+B<-10
+C <- A + B
+C
+
+# Trying out with variables
+v1 <- 4.5
+v2 <-2
+v3 <- v1 / v2
+v3
+
+v4 <- sqrt(v1)
+v4
+
+# Trying out with some More Variables
+greet <-"Hello,"
+name <-" Teena"
+message <- paste(greet, name)
+message
+
+## Trying out with different data types
+# Trying out with integers
+x <- 2L
+typeof(x)
+
+# Trying out with double
+y <- 2.5
+typeof(y)
+
+# Trying out with complex
+z <- 2+3i
+typeof(z)
+
+# Trying out with character
+a <- "h"
+typeof(a)
+
+# Trying out with logical
+q <- T
+typeof(q)
+
+p <- FALSE 
+typeof(p)
+
+# Trying out with Basic Math
+a <- 5
+b <- 3
+c <- a + b
+d <- a - b
+e <- a * b
+f <- a / b
+
+a
+b
+c
+d
+e
+f
+
+# Trying out Basic Logic using operations
+4 <5
+10>100
+4==5
+
+result <- 4 < 5
+result
+typeof(result)
+
+result2 <- !(5>1)
+result2
+
+result | result2
+result & result2
+
+isTRUE(result)
