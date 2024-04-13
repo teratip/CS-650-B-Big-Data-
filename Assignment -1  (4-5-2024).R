cb0e260ee01a624902e353d75170b1915a839f28
teratip <teratipallavi123@gmail.com>
2024-04-12 23:11:33 -0400

    first commit

---
+++ Assignment -1  (4-5-2024).R
@@ -0,0 +1,29 @@
+# 1.Write a program to get the average of 3numbers: 7, 12, and 5
+# let the numbers be x , y , z
+x <- 7
+y <- 12
+z <- 5
+
+# Average calculation
+average <- (x + y + z) / 3
+average
+
+# 2.Write a program to print out “Welcome to R,<your first name>”
+# Print the message
+greet <-"Welcome to R,"
+name <-" Pallavi"
+message <- paste(greet, name)
+message
+
+# 3.Let x be 7, y be 12 and z be 4, print out whether x is bigger than y and whether x is bigger than z
+# The variables
+x <- 7
+y <- 12
+z <- 4
+
+# Print whether x is bigger than y
+x > y
+
+# Print whether x is bigger than z
+x > z
+
